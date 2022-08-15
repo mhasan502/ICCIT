6f5ef9ffa6c071fa81ec8f3f30dff23a2371f362
mhasan502 <mhasan502@gmail.com>
2022-08-15 13:06:48 +0600

    Removed whitenoise as static_storage

--- ICCIT2021/settings.py
+++ ICCIT2021/settings.py
@@ -119,7 +119,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
